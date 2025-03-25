c4743ba28cd60351a68d941cb68832cb0a88d63e
trgnam04 <trungnamtbcm123@gmail.com>
2025-03-25 17:18:47 +0700

    done OTA Update

--- src/main.cpp
+++ src/main.cpp
@@ -8,61 +8,134 @@
 #endif // ESP8266
 
 #include <Arduino_MQTT_Client.h>
-#include <Server_Side_RPC.h>
+#include <OTA_Firmware_Update.h>
 #include <ThingsBoard.h>
-#include <string.h>
 
-#define LED_PIN 13
+#ifdef ESP8266
+#include <Arduino_ESP8266_Updater.h>
+#else
+#ifdef ESP32
+// We do theoretically have an Arduino_ESP32_Updater component, which has dependencies on Arduino and used the UpdaterClass,
+// but it makes not sense to use that component atleast currently, because it simply implements writing to partitions
+// in a very suboptimal way, allocatng 4KB on the heap and even causing undefined behaviour and even memory leaks.
+// See https://github.com/espressif/arduino-esp32/issues/7984#issuecomment-1717151822 for more information on the issues with the UpdaterClass.
+// Therefore instead it is recommended to use the Epsressif_Updater which directly uses the headers, which are included in the UpdaterClass anyway,
+// but because it directly use the OTA Update API see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/ota.html for more information,
+// it is more efficient and does not have any of the aforementioned issues.
+// Thanks to those issues it is highly recommended to use the Espressif_Updater as long as the issue has not been resolved on a Arduino level, additional the same is the case
+// if an older version of Arduino is used that might not contain the possible fixes yet.
+#include <Espressif_Updater.h>
+#endif // ESP32
+#endif // ESP8266
 
 
-constexpr char WIFI_SSID[] = "271104E";
-constexpr char WIFI_PASSWORD[] = "1234567890";
+// Whether the given script is using encryption or not,
+// generally recommended as it increases security (communication with the server is not in clear text anymore),
+// it does come with an overhead tough as having an encrypted session requires a lot of memory,
+// which might not be avaialable on lower end devices.
+#define ENCRYPTED false
 
-constexpr char TOKEN[] = "Button2";
+
+// Firmware title and version used to compare with remote version, to check if an update is needed.
+// Title needs to be the same and version needs to be different --> downgrading is possible
+constexpr char CURRENT_FIRMWARE_TITLE[] = "TEST";
+constexpr char CURRENT_FIRMWARE_VERSION[] = "1.0.0";
+
+// Maximum amount of retries we attempt to download each firmware chunck over MQTT
+constexpr uint8_t FIRMWARE_FAILURE_RETRIES = 12U;
+// Size of each firmware chunck downloaded over MQTT,
+// increased packet size, might increase download speed
+constexpr uint16_t FIRMWARE_PACKET_SIZE = 4096U;
+
+constexpr char WIFI_SSID[] = "v";
+constexpr char WIFI_PASSWORD[] = "12345678";
+
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+constexpr char TOKEN[] = "ESP_OTA_ver1";
 
 // Thingsboard we want to establish a connection too
 constexpr char THINGSBOARD_SERVER[] = "app.coreiot.io";
 
 // MQTT port used to communicate with the server, 1883 is the default unencrypted MQTT port,
 // whereas 8883 would be the default encrypted SSL MQTT port
-#if ENCRYPTED
-constexpr uint16_t THINGSBOARD_PORT = 8883U;
-#else
 constexpr uint16_t THINGSBOARD_PORT = 1883U;
-#endif
 
 // Maximum size packets will ever be sent or received by the underlying MQTT client,
 // if the size is to small messages might not be sent or received messages will be discarded
-constexpr uint16_t MAX_MESSAGE_SEND_SIZE = 256U;
-constexpr uint16_t MAX_MESSAGE_RECEIVE_SIZE = 256U;
+constexpr uint16_t MAX_MESSAGE_SEND_SIZE = 512U;
+constexpr uint16_t MAX_MESSAGE_RECEIVE_SIZE = 512U;
 
-// Baud rate for the debugging serial connection.
+// Baud rate for the debugging serial connection
 // If the Serial output is mangled, ensure to change the monitor speed accordingly to this variable
 constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
 
+#if ENCRYPTED
+// See https://comodosslstore.com/resources/what-is-a-root-ca-certificate-and-how-do-i-download-it/
+// on how to get the root certificate of the server we want to communicate with,
+// this is needed to establish a secure connection and changes depending on the website.
+constexpr char ROOT_CERT[] = R"(-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)";
+#endif
 
-constexpr const char RPC_SWITCH_METHOD[] = "setLedSwitchValue";
-constexpr const char RPC_GET_STATE_METHOD[] = "getInitiateState";
-constexpr const char RPC_SWITCH_KEY[] = "Button2";
-constexpr uint8_t MAX_RPC_SUBSCRIPTIONS = 2U;
-constexpr uint8_t MAX_RPC_RESPONSE = 2U;
-
-volatile bool button_state = false;
 
 // Initialize underlying client, used to establish a connection
+#if ENCRYPTED
+WiFiClientSecure espClient;
+#else
 WiFiClient espClient;
+#endif
 // Initalize the Mqtt client instance
 Arduino_MQTT_Client mqttClient(espClient);
 // Initialize used apis
-Server_Side_RPC<MAX_RPC_SUBSCRIPTIONS, MAX_RPC_RESPONSE> rpc;
+OTA_Firmware_Update<> ota;
 const std::array<IAPI_Implementation*, 1U> apis = {
-    &rpc
+    &ota
 };
 // Initialize ThingsBoard instance with the maximum needed buffer size
 ThingsBoard tb(mqttClient, MAX_MESSAGE_RECEIVE_SIZE, MAX_MESSAGE_SEND_SIZE, Default_Max_Stack_Size, apis);
+// Initalize the Updater client instance used to flash binary to flash memory
+#ifdef ESP8266
+Arduino_ESP8266_Updater updater;
+#else
+#ifdef ESP32
+Espressif_Updater<> updater;
+#endif // ESP32
+#endif // ESP8266
 
-// Statuses for subscribing to rpc
-bool subscribed = false;
+// Statuses for updating
+bool currentFWSent = false;
+bool updateRequestSent = false;
 
 
 /// @brief Initalizes WiFi connection,
@@ -77,13 +150,16 @@ void InitWiFi() {
     Serial.print(".");
   }
   Serial.println("Connected to AP");
+#if ENCRYPTED
+  espClient.setCACert(ROOT_CERT);
+#endif
 }
 
 /// @brief Reconnects the WiFi uses InitWiFi if the connection has been removed
 /// @return Returns true as soon as a connection has been established again
 bool reconnect() {
   // Check to ensure we aren't connected yet
-const wl_status_t status = WiFi.status();
+  const wl_status_t status = WiFi.status();
   if (status == WL_CONNECTED) {
     return true;
   }
@@ -93,25 +169,42 @@ const wl_status_t status = WiFi.status();
   return true;
 }
 
-void processGetInitiateState(const JsonVariantConst &data, JsonDocument &response) {
-  Serial.println("Received the get Initiate State");
-  bool currState = digitalRead(LED_PIN);
-  response.set(currState);
+/// @brief Update starting callback method that will be called as soon as the shared attribute firmware keys have been received and processed
+/// and the moment before we subscribe the necessary topics for the OTA firmware update.
+/// Is meant to give a moment were any additional processes or communication with the cloud can be stopped to ensure the update process runs as smooth as possible.
+/// To ensure that calling the ThingsBoardSized::Cleanup_Subscriptions() method can be used which stops any receiving of data over MQTT besides the one for the OTA firmware update,
+/// if this method is used ensure to call all subscribe methods again so they can be resubscribed, in the method passed to the finished_callback if the update failed and we do not restart the device
+void update_starting_callback() {
+  // Nothing to do
 }
 
+/// @brief End callback method that will be called as soon as the OTA firmware update, either finished successfully or failed.
+/// Is meant to allow to either restart the device if the udpate was successfull or to restart any stopped services before the update started in the subscribed update_starting_callback
+/// @param success Either true (update successful) or false (update failed)
+void finished_callback(const bool & success) {
+  if (success) {
+    Serial.println("Done, Reboot now");
 
-void processSwitchChange(const JsonVariantConst &data, JsonDocument &response) {
-  Serial.println("Received the set switch method");
-  
-  bool switch_state = data.as<String>() == "true" ? 1 : 0;
-  button_state = switch_state;
-
-  Serial.print("Example switch state: ");
-  Serial.println(button_state ? "ON" : "OFF");
-
-  digitalWrite(LED_PIN, button_state);  
+#ifdef ESP8266
+    ESP.restart();
+#else
+#ifdef ESP32
+    esp_restart();
+#endif // ESP32
+#endif // ESP8266
+    return;
+  }
+  Serial.println("Downloading firmware failed");
 }
 
+/// @brief Progress callback method that will be called every time our current progress of downloading the complete firmware data changed,
+/// meaning it will be called if the amount of already downloaded chunks increased.
+/// Is meant to allow to display a progress bar or print the current progress of the update into the console with the currently already downloaded amount of chunks and the total amount of chunks
+/// @param current Already received and processs amount of chunks
+/// @param total Total amount of chunks we need to receive and process until the update has completed
+void progress_callback(const size_t & current, const size_t & total) {
+  Serial.printf("Progress %.2f%%\n", static_cast<float>(current * 100U) / total);
+}
 
 void setup() {
   // Initalize serial connection for debugging
@@ -122,8 +215,7 @@ void setup() {
 
 void loop() {
   delay(1000);
-
-  digitalWrite(LED_PIN, button_state ? HIGH : LOW);
+  
 
   if (!reconnect()) {
     return;
@@ -139,24 +231,19 @@ void loop() {
     }
   }
 
-  if (!subscribed) {
-    Serial.println("Subscribing for RPC...");
-    const std::array<RPC_Callback, MAX_RPC_SUBSCRIPTIONS> callbacks = {
-       // Internal size can be 0, because if we use the JsonDocument as a JsonVariant and then set the value we do not require additional memory
-      RPC_Callback{ RPC_SWITCH_METHOD,         processSwitchChange },
-      RPC_Callback{ RPC_GET_STATE_METHOD,      processGetInitiateState}
-    };
-    
-    if (!rpc.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
-      Serial.println("Failed to subscribe for RPC");
-      return;
-    }
-
-    Serial.println("Subscribe done");
-    subscribed = true;    
+  if (!currentFWSent) {
+    currentFWSent = ota.Firmware_Send_Info(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION);
   }
 
-
+  if (!updateRequestSent) {
+    Serial.println("Firwmare Update Subscription...");
+    const OTA_Update_Callback callback(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION, &updater, &finished_callback, &progress_callback, &update_starting_callback, FIRMWARE_FAILURE_RETRIES, FIRMWARE_PACKET_SIZE);
+    // See https://thingsboard.io/docs/user-guide/ota-updates/
+    // to understand how to create a new OTA pacakge and assign it to a device so it can download it.
+    // Sending the request again after a successfull update will automatically send the UPDATED firmware state,
+    // because the assigned firmware title and version on the cloud and the firmware version and title we booted into are the same.
+    updateRequestSent = ota.Subscribe_Firmware_Update(callback);
+  }
 
   tb.loop();
 }
